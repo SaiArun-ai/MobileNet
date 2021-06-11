a2d541c416b8939c3fc83a907afca848955b5a72
SaiArun-ai <72151107+SaiArun-ai@users.noreply.github.com>
2021-06-11 19:33:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,3 +10,29 @@ Webcam.set({
 
 camera = document.getElementById("camera");
 Webcam.attach("#Camera");
+
+function CaptureImage(){
+    Webcam.snap(function (data_uri){
+        document.getElementById("Result").innerHTML = '<img id = "ig" src = "'  + data_uri + '" width = 250 height = 200> ';
+    });
+    
+}
+
+var Classifier = ml5.imageClassifier('Mobilenet',modelLoaded);
+function modelLoaded(){
+
+}
+function Identify(){
+    image = document.getElementById("ig").value;
+    Classifier.classify(image,gotresult);
+}
+
+
+function gotresult(error,results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("WordLaResult").innerHTML = results[0].label;
+    }
+}
